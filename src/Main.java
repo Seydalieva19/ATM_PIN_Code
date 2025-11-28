70300e083f36547ab7f9b588d19832d5b7146e39
Sheyda Aliyeva <seydalieva2004@gmail.com>
2025-11-28 19:15:37 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,3 +24,37 @@ public class Main{
        }
 
     }
+
+
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int pinCode = 1234;
+//         int enterPIN;
+//         int attempts = 3;
+
+//         while (attempts > 0) {
+//             System.out.print("PIN daxil edin: ");
+//             enterPIN = sc.nextInt();
+
+//             if (enterPIN == pinCode) {
+//                 System.out.println("Sistemə daxil oldunuz!");
+//                 break;
+//             } else {
+//                 attempts--;
+//                 if (attempts > 0) {
+//                     System.out.println("Yanlış PİN. Yenidən cəhd edin");
+//                     System.out.println("Qalib cəhd sayı: " + attempts);
+//                 } else {
+//                     System.out.println("Kartınız bloklandı!");
+//                 }
+//             }
+//         }
+
+//         sc.close();
+//     }
+// }
